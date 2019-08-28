e7d29b7fd283aa190c95fd32b9056d1e87b645a7
Johir Rayhan <johir.rayhan01@gmail.com>
2019-08-28 12:36:26 +0600

    Example Object

---
+++ src/modal/test.js
@@ -0,0 +1,19 @@
+ export default {
+    kitchenWooden: false,
+    kitchenAluminium: false,
+    kitchen: false,
+    barbeque: false,
+    centralConditioning: false,
+    doubleGlaze: false,
+    sunHeater: false,
+    waterWell: false,
+    houseStore: false,
+    washRoom: false,
+    barbecueArea: false,
+    parking: false,
+    parking2: false,
+    swimmingPool: false,
+    swimmingPool2: false,
+    playArea: false,
+    parkings: false
+}
